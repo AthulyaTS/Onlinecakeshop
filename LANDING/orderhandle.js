2225afce1b5c8f2acb2061e7d9fde0d891523757
Athulyats <tsathulya78@gmail.com>
2022-08-02 13:57:44 +0530

    UDDATED

--- LANDING/orderhandle.js
+++ LANDING/orderhandle.js
@@ -22,14 +22,21 @@ const SendOrder = (endpoint, item_name, item_price,itemquantity_id) => {
         order_date:"2/5/22"
     };
     console.log(JSON.stringify(obj));
-    //  postData("http://localhost:4000/"+endpoint, obj)
-    // .then((data) => {
-    // console.log(data); // JSON data parsed by `data.json()` call
-    // });
+      postData("http://localhost:4000/"+endpoint, obj)
+     .then((data) => {
+     console.log(data); // JSON data parsed by `data.json()` call
+     });
 
 };
 
-
+// id: 4
+// item_name: "BLACKFOREST"
+// item_price: "200"
+// item_quantity: "2"
+// order_date: "2/5/22"
+// user_full_address: "ABCD"
+// user_full_name: "Aparna MS"
+// user_phone: "1236554"
 
 
 
@@ -41,7 +48,24 @@ const SendOrder = (endpoint, item_name, item_price,itemquantity_id) => {
    await fetch("http://localhost:4000/"+endpoint)
          .then((response) => response.json())
          .then((data) => dataBack=data);
+         console.log(dataBack);
+   
    var data = ``;
+  dataBack.forEach((each,key)=>{
+    console.log(each);
+    data+= `<tr>
+    <td>${key}</td>
+    <td>${each.item_name}</td>
+    <td>${each.item_price}</td>
+    <td>${each.item_quantity}</td>
+    <td>${each.order_date}</td>
+    <td>${each.user_full_address}</td>
+    <td>${each.user_full_name}</td>
+    <td>${each.user_phone}</td>
+   </tr>`;
+   })
+  document.getElementById(elementID).innerHTML=data;
+
  
  }
  
--- LANDING/updatehandler.js
+++ LANDING/updatehandler.js
@@ -1,18 +1,24 @@
-const UpdateItem = (endpoint, price,status) => {
+const UpdateItem = (endpoint) => {
     console.log("clicked");
+    var name = document.getElementById("cakenamein").value;
+    var price= document.getElementById("pricein").value;
+    var status= document.getElementById("numin").value;
+    // console.log(name);
+    // console.log(price);
+    // console.log(status);
     
     var obj ={
-      pname:"BLACKFOREST",
-      price:"750",
-      status:"available"
+      pname:name,
+      price:price,
+      status:status
         };
 
     console.log(JSON.stringify(obj));
-    //  postData("http://localhost:4000/"+endpoint, obj)
-    // .then((data) => {
-    // console.log(data); // JSON data parsed by `data.json()` call
-    // });
-
+      patchdata("http://localhost:4000/"+endpoint, obj)
+       .then((data) => {
+     console.log(data); // JSON data parsed by `data.json()` call
+      });
+location.reload()
 };
 
 
@@ -21,17 +27,29 @@ const GetData = async (elementID, endpoint) =>{
     await fetch("http://localhost:4000/"+endpoint)
           .then((response) => response.json())
           .then((data) => dataBack=data);
-    var data = ``;
-  
+   
+   var cakedata=dataBack[0];
+   console.log(cakedata)
+   var data = `<tr><br><br>
+   <td>1</td><br>
+   <td>100</td>
+
+   <td>${cakedata.pname}</td>
+   <td>${cakedata.price}</td>
+   <td>${cakedata.status}</td>
+
+  </tr>`;
+ 
+   document.getElementById(elementID).innerHTML=data;
   }
   
  
  //QUERY CALLS 
  // Example POST method implementation:
- async function postData(url = '', data = {}) {
+ async function patchdata(url = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
